a19f004c7471e2172247db01a80012fd8bd40170
AKSarder <63703796+AKSarder@users.noreply.github.com>
2022-04-19 16:16:35 +0600

    done

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import logo from './logo.svg';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './App.css';
+import Restaurant from './components/Restaurant/Restaurant';
+import Header from './components/Header/Header';
+import { Button } from 'react-bootstrap';
+
+function App() {
+  return (
+    <div className="App">
+
+      <Button variant='primary'>Click me</Button>
+
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Components/Entry/Entry.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+const Entry = () => {
+    return (
+        <div>
+            <EntryFrom></EntryFrom>
+        </div>
+    );
+};
+
+
+function EntryFrom() {
+    <form action="">
+        <input type="text" />Name
+        <input type="text" />Action
+        <input type="text" />Time
+    </form>
+
+}
+export default Entry;
